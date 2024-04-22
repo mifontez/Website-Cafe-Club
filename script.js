1871873d90c15852124bf547e79dfe7df4de4fa3
Miguel Fontes <155191516+mifontez@users.noreply.github.com>
2024-04-21 23:52:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active'); /* aqui*/
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active'); /*isso é como se fosse um botão ON/OFF, ele verifica se está ativo, caso sim, ele prossegue com a ação*/
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
